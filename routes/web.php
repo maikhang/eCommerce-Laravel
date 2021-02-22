88b7b2f44277414efb7b88e6b8ed53f03f8f108d
Mai Khang <mhkhang.khmt0117@student.ctuet.edu.vn>
2021-02-22 15:21:57 +0700

    Admin and User Authentication

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\LiveWire\HomeComponent;
 use App\Http\LiveWire\ShopComponent;
 use App\Http\LiveWire\CartComponent;
 use App\Http\LiveWire\CheckoutComponent;
+use App\Http\LiveWire\Admin\AdminDashboardComponent;
+use App\Http\LiveWire\User\UserDashboardComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +19,23 @@ use App\Http\LiveWire\CheckoutComponent;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// [GET] Site Route
 Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
+
+// [AUTH] User
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+// [AUTH] Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+// [AUTH] User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
